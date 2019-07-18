a783fe6d1bef39abba0e5426bee659469c39013b
Sam Markham <sam.e.markham@gmail.com>
2019-07-18 18:25:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,5 +1,6 @@
 class Transfer
-  attr_reader :sender, :receiver, :status, :amount
+  attr_reader :sender, :receiver, :amount
+  attr_accessor :status 
   def initialize(sender, receiver, amount)
     @sender = sender
     @receiver = receiver
@@ -16,6 +17,11 @@ class Transfer
   end
 
   def execute_transaction
-    
+    if self.valid? 
+      sender.withdraw(@amount)
+      receiver.deposit(@amount)
+      self.status = "complete"
+    else 
+      ""
   end
 end
