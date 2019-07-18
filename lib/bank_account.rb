25623d681d8b63394981862292827df090442bf3
Sam Markham <sam.e.markham@gmail.com>
2019-07-18 18:11:40 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,5 +1,5 @@
 class BankAccount
-  attr_reader :name, :balance, :status
+  attr_reader :name, :balance, :status 
   def initialize(name)
     @name = name
     @balance = 1000
@@ -8,5 +8,9 @@ class BankAccount
 
   def deposit(amount)
     @balance += amount 
-  end
+  end 
+
+  def display_balance 
+    "Your balance is $#{self.balance}."
+  end 
 end
